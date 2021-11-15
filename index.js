4f7fe82fd8f9d23e86241ef94972cb5bb731cbab
Abu Salek <bhairabbari330@gmail.com>
2021-11-15 13:14:05 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,136 @@
+const { MongoClient } = require('mongodb');
+const express = require('express')
+const app = express()
+const cors = require('cors');
+const admin = require("firebase-admin");
+const port = process.env.PORT || 5000
+require('dotenv').config()
+
+const serviceAccount = require('./doctors-portal-new-firebase-adminsdk.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+// middlewere
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zd73h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function verifyToken (req, res, next) {
+  if(req.headers?.authorization?.startsWith('Bearer ')) {
+    const token = req.headers.authorization.split(' ')[1]
+
+
+    try{
+      const decodedUser = await admin.auth().verifyIdToken(token)
+      req.decodedEmail = decodedUser.email
+    }
+    catch{
+
+    }
+
+  }
+
+  next()
+}
+
+async function run() {
+    try {
+      await client.connect();
+      // create database
+      const database = client.db("doctors_portal")
+      // create collection
+      const appointmentCollection = database.collection("appointment")
+      const usersCollection = database.collection("users")
+
+      // get method
+      // app.get('/appointments', async(req, res) =>{
+      //   const cursor = appointmentCollection.find({})
+      //   const appointments = await cursor.toArray()
+      //   res.send(appointments)
+      // })
+
+      // get method
+      app.get('/appointments', async(req, res) => {
+        const email = req.query.email
+        const date = new Date(req.query.date).toLocaleDateString()
+        // console.log(date)
+        // console.log(email)
+        const query = {email: email, date: date}
+        const cursor = appointmentCollection.find(query)
+        const appointments = await cursor.toArray()
+        res.json(appointments)
+      })
+
+      app.get('/users/:email', async(req, res) => {
+        const email = req.params.email
+        const query = {email: email}
+        const user = await usersCollection.findOne(query)
+        let isAdmin = false
+        if(user?.role === 'admin') {
+          isAdmin = true
+        }
+        res.json({admin: isAdmin})
+      })
+
+      // post appointment with post method
+      app.post('/appointments', async (req, res) => {
+        const appointment = req.body;
+        const result = await appointmentCollection.insertOne(appointment);
+        res.json(result)
+    });
+
+    app.post('/users', async(req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user)
+      console.log(result)
+      res.json(result)
+    })
+
+    app.put('/users', async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const options = { upsert: true };
+      const updateDoc = { $set: user };
+      const result = await usersCollection.updateOne(filter, updateDoc, options);
+      res.json(result);
+  });
+
+  // put method for make admin
+    app.put('/users/admin', verifyToken, async(req, res) => {
+      const user = req.body
+
+      const requester = req.decodedEmail
+      if(requester) {
+        const requesterAccount = await usersCollection.findOne({email: requester})
+        if(requesterAccount.role === 'admin') {
+          const filter = {email: user.email}
+          const updateDoc = { $set: {role: 'admin'}}
+          const result = await usersCollection.updateOne(filter, updateDoc)
+          res.json(result)
+        }
+      } 
+      else {
+        res.status(403).json({message: 'you do not have access to make admin'})
+      }
+      
+    })
+
+    } finally {
+    //   await client.close();
+    }
+  }
+  run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('Hello Doctors Portal')
+  console.log('hi')
+})
+
+app.listen(port, () => {
+  console.log(`istening ${port}`)
+})
